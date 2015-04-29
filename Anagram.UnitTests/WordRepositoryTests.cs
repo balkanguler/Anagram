7919baef94f15cc385cfe4591ea661988072db1a
Balkan Güler <cezeri@gmail.com>
2015-04-29 09:08:21 +0300

    Repository

---
+++ Anagram.UnitTests/WordRepositoryTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Anagram.UnitTests
+{
+    [TestFixture]
+    public class WordRepositoryTests
+    {
+        [Test]
+        public void ReturnsTrueForAWordInRepository()
+        {
+            string word = "word";
+            WordRepository repository = new WordRepository(new string[] { word });
+
+            Assert.IsTrue(repository.Contains(word));
+        }
+    }
+}
---
+++ Anagram/WordRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Anagram
+{
+    public class WordRepository
+    {
+        private List<string> wordList;
+
+        public WordRepository(string[] words)
+        {
+            wordList = new List<string>();
+            wordList.AddRange(words);
+        }
+
+        public bool Contains(string word)
+        {
+            return wordList.Contains(word);
+        }
+    }
+}
