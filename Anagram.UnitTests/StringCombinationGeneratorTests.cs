d8f484e66368bf2d189f6698a5b4c4adb5a44eee
Balkan Güler <cezeri@gmail.com>
2015-04-29 11:05:13 +0300

    StringCombinationGenerator added.

---
+++ Anagram.UnitTests/StringCombinationGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Anagram;
+using System.Collections.Generic;
+
+namespace Anagram.UnitTests
+{
+    [TestFixture]
+    public class StringCombinationGeneratorTests
+    {
+        [Test]
+        public void GeneratesSameLengthCombinationOfAString()
+        {
+            var word = "ab";
+
+            StringCombinationGenerator generator = new StringCombinationGenerator();
+            List<string> combinations = generator.GenerateSameLengthCombinations(word);
+
+            Assert.AreEqual(1, combinations.Count);
+            Assert.IsTrue(combinations.Contains("ba"));
+        }
+    }
+}
---
+++ Anagram/StringCombinationGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Anagram
+{
+    public class StringCombinationGenerator
+    {
+        public List<string> GenerateSameLengthCombinations(string word)
+        {
+            return new List<string> { "ba" };
+        }
+    }
+}
