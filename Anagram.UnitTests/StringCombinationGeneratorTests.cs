eca208516e046ebd367713505e7258faa6d2d7e2
Balkan Güler <cezeri@gmail.com>
2015-04-29 14:29:05 +0300

    SringCombinationGenerater Added

--- Anagram.UnitTests/StringCombinationGeneratorTests.cs
+++ Anagram.UnitTests/StringCombinationGeneratorTests.cs
@@ -18,5 +18,27 @@ namespace Anagram.UnitTests
             Assert.AreEqual(1, combinations.Count);
             Assert.IsTrue(combinations.Contains("ba"));
         }
+
+        [Test]
+        public void DoesNotGenerateSameWord()
+        {
+            var word = "ab";
+
+            StringCombinationGenerator generator = new StringCombinationGenerator();
+            List<string> combinations = generator.GenerateSameLengthCombinations(word);
+
+            Assert.IsFalse(combinations.Contains(word));
+        }
+
+        [Test]
+        public void DoesNotGeneratesACombinationMoreThanOnce()
+        {
+            var word = "abc";
+
+            StringCombinationGenerator generator = new StringCombinationGenerator();
+            List<string> combinations = generator.GenerateSameLengthCombinations(word);
+
+            combinations.ForEach(c => Assert.AreEqual(combinations.FindAll(i => i == c).Count, 1));
+        }
     }
 }
--- Anagram/StringCombinationGenerator.cs
+++ Anagram/StringCombinationGenerator.cs
@@ -9,7 +9,34 @@ namespace Anagram
     {
         public List<string> GenerateSameLengthCombinations(string word)
         {
-            return new List<string> { "ba" };
+            char[] charsOfWord = word.ToCharArray();
+            List<string> combinations = new List<string>();
+
+            for (int i = 0; i < charsOfWord.Length; i++)
+            {
+                char currentChar = charsOfWord[i];
+
+                string leftString = string.Empty;
+                string rightString = string.Empty;
+
+                if (i > 0)
+                    leftString = word.Substring(0, i);
+
+                if (i < word.Length - 1)
+                    rightString = word.Substring(i + 1, word.Length - (i + 1));
+
+
+                string wordWithOutCurrentChar = leftString + rightString;
+
+                for (int j = 0; j < wordWithOutCurrentChar.Length; j++)
+                {
+                    string combination = wordWithOutCurrentChar.Insert(j, currentChar.ToString());
+                    if (!combinations.Contains(combination) && !word.Equals(combination))
+                        combinations.Add(combination);
+                }
+            }
+
+            return combinations;
         }
     }
 }
