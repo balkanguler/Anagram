8f476a032e3368e5cb6668aa69e18b44cce16ee7
Balkan Güler <cezeri@gmail.com>
2015-04-29 08:42:49 +0300

    First test passed

---
+++ Anagram.UnitTests/AnagramFinderTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Anagram;
+using System.Collections.Generic;
+
+namespace Anagram.UnitTests
+{
+    [TestFixture]
+    public class AnagramFinderTests
+    {
+        [Test]
+        public void FindsAnagramsOfAWord()
+        {
+            AnagramFinder finder = new AnagramFinder();
+            var word = "wired";
+
+            string[] anagrams = finder.Find(word);
+
+            Assert.That(new List<string>(anagrams).Contains("wierd"), Is.True);
+
+        }
+    }
+}
---
+++ Anagram/AnagramFinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Anagram
+{
+    public class AnagramFinder
+    {
+        public string[] Find(string word)
+        {
+            return new string[] { "wierd" };
+        }
+    }
+}
