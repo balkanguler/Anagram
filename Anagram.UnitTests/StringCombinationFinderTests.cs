266caf154c960029e59af063e1d77cbed5b4d77f
Balkan Güler <cezeri@gmail.com>
2015-04-30 20:58:14 +0300

    StringCombinationFinder and tests are added. For long words we'll not generate all combinations. Instead we will check the same length words in repository.

---
+++ Anagram.UnitTests/StringCombinationFinderTests.cs
@@ -0,0 +1,37 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram.UnitTests
+{
+    [TestFixture]
+    public class StringCombinationFinderTests
+    {
+
+        [Test]
+        public void FindsCombinationsOnlyInListGiven()
+        {
+            IWordRepository wordRepository = Substitute.For<IWordRepository>();
+
+            var word = "cdeab";
+            var anagrams = new List<string>{"abcde", "bcdea", "edcba"};
+            var existingWords = new List<string>{"12345", "xyzte"};
+            existingWords.AddRange(anagrams);
+
+            wordRepository.GetWordsWithLength(5).Returns(existingWords);
+
+            StringCombinationFinder finder = new StringCombinationFinder(wordRepository);
+
+            var foundCombinations = finder.ProvideSameLengthCombinations(word);
+
+            Assert.AreEqual(anagrams.Count, foundCombinations.Count);
+
+            anagrams.ForEach(a => Assert.IsTrue(foundCombinations.Contains(a)));
+
+        }
+    }
+}
