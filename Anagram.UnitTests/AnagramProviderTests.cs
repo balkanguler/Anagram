e2aae08064c1f38a73bd480cc3948ae2d9e58648
Balkan Güler <cezeri@gmail.com>
2015-05-04 16:21:55 +0300

    Untracked files add to git

---
+++ Anagram.UnitTests/AnagramProviderTests.cs
@@ -0,0 +1,47 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anagram;
+
+namespace Anagram.UnitTests
+{
+    [TestFixture]
+    public class AnagramProviderTests
+    {
+        [Test]
+        [TestCase("a")]
+        [TestCase("ab")]
+        [TestCase("abc")]
+        [TestCase("abcd")]
+        [TestCase("abcde")]
+        public void UsesDeterministicAlgorithmForShortWords(string word)
+        {
+            IWordRepository repository = Substitute.For<IWordRepository>();
+
+            AnagramProvider provider = new AnagramProvider(repository);
+
+            provider.FindAnagrams(word);
+
+            repository.DidNotReceive().GetWordsWithLength(word.Length);
+        }
+
+        [Test]
+        [TestCase("abcdef")]
+        [TestCase("abcdefg")]
+        public void UsesHeuristicAlgorithmForShortWords(string word)
+        {
+            IWordRepository repository = Substitute.For<IWordRepository>();
+
+            AnagramProvider provider = new AnagramProvider(repository);
+
+            provider.FindAnagrams(word);
+
+            repository.Received(1).GetWordsWithLength(word.Length);
+        }
+        
+    }
+}
